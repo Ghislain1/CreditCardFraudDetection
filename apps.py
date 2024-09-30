f639394ab41baf409d36c4df70396b38c3ea3c21
Ghislain Z. <gzeleu@outlook.de>
2024-09-30 23:48:39 +0200

    Find best Model for prediction

--- apps.py
+++ apps.py
@@ -1,13 +1,13 @@
-import pandas as pd
 from check_data import CheckData
 from check_unbalanced_data import Check_Unbalanced_Data
+from prediction import Prediction
 
 
 class App:
     def __init__(self):
         self.check_data = CheckData()  # erstellen ein Objekt der CheckData-Klasse
 
-    '''Auf der CheckData-Methode und Ausgabe der Ergebnisse'''
+    """Auf der CheckData-Methode und Ausgabe der Ergebnisse"""
 
     def run(self):
         print("erste 5 Zeile:")
@@ -22,14 +22,19 @@ class App:
         print("Überprüfung der fehlenden daten:")
         print(self.check_data.check_missing_data())
 
-        '''Erstellen ein Objekt der Check_Unbalanced_Data und Aufruf der methode'''
+        """Erstellen ein Objekt der Check_Unbalanced_Data und Aufruf der methode"""
         unbalanced_checker = Check_Unbalanced_Data(self.check_data.data_df)
         unbalanced_checker.plot_class_distribution()
 
         print(self.check_data.show_class_column())
 
+    def call_prediction(self):
+        prediction = Prediction()
+        prediction.run()
 
-'''Ausführung der App-Klasse'''
+
+"""Ausführung der App-Klasse"""
 if __name__ == "__main__":
     app = App()  # Erstellung ein Main-Objekt
-    app.run()  # Ausführung der main-Mathode
+    # app.run()  # Ausführung der main-Mathode
+    app.call_prediction()
